255f72f8202bfa6ff078a3f096be0e234970c63d
Navaneeth D <144593800+ar3s-nd@users.noreply.github.com>
2025-12-07 20:03:53 +0530

    Update BaseIntegrationTest.java

--- src/test/java/com/v322/healthsync/BaseIntegrationTest.java
+++ src/test/java/com/v322/healthsync/BaseIntegrationTest.java
@@ -1,32 +1,32 @@
-// package com.v322.healthsync;
+package com.v322.healthsync;
 
-// import org.springframework.boot.test.context.SpringBootTest;
-// import org.springframework.test.context.DynamicPropertyRegistry;
-// import org.springframework.test.context.DynamicPropertySource;
-// import org.testcontainers.containers.MySQLContainer;
-// import org.testcontainers.junit.jupiter.Container;
-// import org.testcontainers.junit.jupiter.Testcontainers; 
-// import org.junit.jupiter.api.Test;
-// import org.testcontainers.containers.MySQLContainer;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers; 
+import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.MySQLContainer;
 
 
-// @Testcontainers
-// @SpringBootTest
-// public abstract class BaseIntegrationTest {
+@Testcontainers
+@SpringBootTest
+public abstract class BaseIntegrationTest {
 
-//     @Container
-//     static MySQLContainer<?> mysql =
-//             new MySQLContainer<>("mysql:8.0")
-//                     .withDatabaseName("healthsync_test")
-//                     .withUsername("test")
-//                     .withPassword("test");
+    @Container
+    static MySQLContainer<?> mysql =
+            new MySQLContainer<>("mysql:8.0")
+                    .withDatabaseName("healthsync_test")
+                    .withUsername("test")
+                    .withPassword("test");
 
-//     @DynamicPropertySource
-//     static void configure(DynamicPropertyRegistry registry) {
-//         registry.add("spring.datasource.url", mysql::getJdbcUrl);
-//         registry.add("spring.datasource.username", mysql::getUsername);
-//         registry.add("spring.datasource.password", mysql::getPassword);
-//         registry.add("spring.datasource.driver-class-name", () -> "com.mysql.cj.jdbc.Driver");
-//         registry.add("spring.jpa.hibernate.ddl-auto", () -> "create");  
-//     }
-// }
+    @DynamicPropertySource
+    static void configure(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", mysql::getJdbcUrl);
+        registry.add("spring.datasource.username", mysql::getUsername);
+        registry.add("spring.datasource.password", mysql::getPassword);
+        registry.add("spring.datasource.driver-class-name", () -> "com.mysql.cj.jdbc.Driver");
+        registry.add("spring.jpa.hibernate.ddl-auto", () -> "create");  
+    }
+}
